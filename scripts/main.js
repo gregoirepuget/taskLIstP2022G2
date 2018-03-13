e21c75f8b252c284ebf9e0be75f78f342a8cc5a3
gregoirepuget <gregoirepuget@gmail.com>
2018-03-13 15:19:05 +0100

    ajout rectab

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,16 @@ const taskContainer = document.querySelector("#listeTaches")
 const addTaskButton = document.querySelector("#ajouterTache")
 const defaultTask = "Tâche"
 
+//let tab = [10,20,23,413,342]
+//localStorage.setItem("tableau",JSON.stringify(tab))
+
+//console.log(JSON.parse(localStorage.getItem("tableau")))
+
 addTaskButton.addEventListener('click', function(e){
   e.preventDefault()
   addTask(defaultTask)
 })
+
 function addTask(taskText){
   /***
       Contenu à créer :
@@ -28,7 +34,10 @@ function addTask(taskText){
   initButton.classList.add('tacheInit')
   initButton.setAttribute('href','#')
   initButton.setAttribute('title','reset')
-  initButton.innerHTML = 'Reset'
+
+  //equivalent de initButton.innerHTML = 'Reset'
+  let initButtonContent = document.createTextNode('Reset')
+  initButton.appendChild(initButtonContent)
 
   let deleteButton = document.createElement('a')
   deleteButton.classList.add('tacheSupprimer')
@@ -44,4 +53,25 @@ function addTask(taskText){
   taskDiv.appendChild(initButton)
   taskDiv.appendChild(deleteButton)
   taskDiv.appendChild(clear)
+
+  // On met à jour le localSTorage
+  recTaskList()
+}
+
+function recTaskList(){
+  // Récupérer toutes les balises qui ont la class "tacheContenu"
+
+  let taskListcontent = document.querySelectorAll('.tacheContenu')
+
+  //On parcourt tout ces éléments et on enregistre leur contenu dans un tableau
+  let tempTab =[]
+   for (let i = 0; i < taskListcontent.length; i++) {
+     tempTab.push(taskListcontent[i].innerHTML)
+   }
+
+  //On convertit notre tableau dans une chaîne de caractère JSON
+  tempTab = JSON.stringify(tempTab)
+
+  // On enregistre notre tableau en localStorage
+  localStorage.setItem('localTaskList', tempTab)
 }
